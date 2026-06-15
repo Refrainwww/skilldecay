caf8b4d2cf9fa0cc4251534343e62b1e037e6136
Zifan Wang <wangzifan_yolo@163.com>
2026-06-16 06:22:07 +0800

    Add benchmarks/skill_debt_bench/__init__.py

---
+++ benchmarks/skill_debt_bench/__init__.py
@@ -0,0 +1,5 @@
+﻿"""SkillDebtBench: controllable skill-debt benchmark."""
+
+from .simulator import BenchmarkConfig, BenchmarkResult, RetrievalMode, run_benchmark
+
+__all__ = ["BenchmarkConfig", "BenchmarkResult", "RetrievalMode", "run_benchmark"]
