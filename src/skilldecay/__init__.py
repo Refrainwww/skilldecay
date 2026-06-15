54a6b1c4abb05e2f23751c539cc5087886269fea
Zifan Wang <wangzifan_yolo@163.com>
2026-06-16 06:22:23 +0800

    Add src/skilldecay/__init__.py

---
+++ src/skilldecay/__init__.py
@@ -0,0 +1,14 @@
+﻿"""SkillDecay: self-forgetting skill library maintenance."""
+
+from .core import Skill, SkillDebtType, SkillEvent, SkillLibrary, SkillState
+from .policy import DecayPolicy, MaintenanceDecision
+
+__all__ = [
+    "DecayPolicy",
+    "MaintenanceDecision",
+    "Skill",
+    "SkillDebtType",
+    "SkillEvent",
+    "SkillLibrary",
+    "SkillState",
+]
